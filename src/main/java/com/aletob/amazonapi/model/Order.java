fea47b5e1f32c3b2ce849e60852b0d073a72a7c3
satse13 <atobiasangeles@gmail.com>
2024-10-08 23:13:14 +0200

    Add Order model class

---
+++ src/main/java/com/aletob/amazonapi/model/Order.java
@@ -0,0 +1,68 @@
+package com.aletob.amazonapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Long userId;
+	private Long productId;
+	private int quantity;
+
+	
+	public Order() {
+	}
+	
+	public Order(Long id, Long userId, Long productId, int quantity) {
+		this.id = id;
+		this.userId = userId;
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
+	public Long getProductId() {
+		return productId;
+	}
+	
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", userId=" + userId + ", productId=" + productId + ", quantity=" + quantity + "]";
+	}
+
+}
