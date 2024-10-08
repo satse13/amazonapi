57006016d8703b181cc6f8f23087472187bcc985
satse13 <atobiasangeles@gmail.com>
2024-10-09 00:18:23 +0200

    Refactor UserService interface to include methods for retrieving, registering, updating, and deleting users

--- src/main/java/com/aletob/amazonapi/service/UserService.java
+++ src/main/java/com/aletob/amazonapi/service/UserService.java
@@ -1,5 +1,15 @@
 package com.aletob.amazonapi.service;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+
+import com.aletob.amazonapi.dto.UserDTO;
+@Service
+public interface UserService {
 	
+	UserDTO getUserById(Long id);
+	UserDTO getUserByEmail(String email);
+	UserDTO registerUser(UserDTO userDTO);
+	UserDTO updateUser(UserDTO userDTO);
+	UserDTO deleteUser(UserDTO userDTO);
+
 }
