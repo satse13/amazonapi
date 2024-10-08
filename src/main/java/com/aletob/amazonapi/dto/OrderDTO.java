2063a03529355f6b2ff72fce737002ce3a3d8ca6
satse13 <atobiasangeles@gmail.com>
2024-10-08 23:54:23 +0200

    Add OrderDTO, ProductDTO, and UserDTO classes

---
+++ src/main/java/com/aletob/amazonapi/dto/OrderDTO.java
@@ -0,0 +1,48 @@
+package com.aletob.amazonapi.dto;
+
+public class OrderDTO {
+	
+	private Long userId;
+	private Long productId;
+	private int quantity;
+
+
+	public OrderDTO() {
+	}
+
+	public OrderDTO(Long userId, Long productId, int quantity) {
+		this.userId = userId;
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderDTO [userId=" + userId + ", productId=" + productId + ", quantity=" + quantity + "]";
+	}
+
+}
---
+++ src/main/java/com/aletob/amazonapi/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.aletob.amazonapi.dto;
+
+public class UserDTO {
+	
+	
+	private String firstName;
+	private String secondName;
+	private String email;
+	
+	
+	public UserDTO() {
+	}
+
+	public UserDTO(String firstName, String secondName, String email) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [firstName=" + firstName + ", secondName=" + secondName + ", email=" + email + "]";
+	}
+	
+}
